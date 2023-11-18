caa6305c87f6b3d019ca8ad922e2342c8d8f7e88
jun9898 <nice1998@gmail.com>
2023-11-18 17:36:16 +0900

    JPA 기초 학습 완료 리뷰 후 심화 학습 시작하기

--- ex2-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex2-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -1,9 +1,6 @@
 package hellojpa;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.EntityTransaction;
-import jakarta.persistence.Persistence;
+import jakarta.persistence.*;
 
 
 public class JpaMain {

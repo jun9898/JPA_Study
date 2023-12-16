aae43390b4eac14451a56a0b29536df5f78b9a94
jun9898 <nice1998@gmail.com>
2023-12-16 21:01:55 +0900

    JPA 기초 학습 EntityManager 사용과 Test코드 작성

---
+++ 스프링 부트와 JPA 활용 1/jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,45 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    // 회원 가입
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다");
+        }
+    }
+
+    // 회원 전체 조회
+    @Transactional(readOnly = true)
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    // 조회를 위한 서비스에는 readOnly true를 설정해주자 (성능적으로 많은 이점을 가져감)
+    @Transactional(readOnly = true)
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+
+
+}
---
+++ 스프링 부트와 JPA 활용 1/jpashop/src/test/java/jpabook/jpashop/repository/MemberRepositoryTest.java
@@ -0,0 +1,39 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    void 회원가입() throws Exception {
+        //given
+        Member member = new Member();
+        member.setName("kim");
+
+        //when
+        Long saveId = memberService.join(member);
+        System.out.println("saveId = " + saveId);
+
+        //then
+        assertEquals(member, memberRepository.findOne(saveId));
+
+    }
+
+
+
+}

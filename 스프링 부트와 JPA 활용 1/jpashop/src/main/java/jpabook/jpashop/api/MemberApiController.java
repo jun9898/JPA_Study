355fb20aed580a1e6fb4206bc5fd2ce98b7b0202
jun9898 <nice1998@gmail.com>
2023-12-23 23:25:36 +0900

    JPA활용 2편 DTO 클래스의 활용 방법과 사용해야 하는 이유에 대해 학습함

---
+++ 스프링 부트와 JPA 활용 1/jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -0,0 +1,105 @@
+package jpabook.jpashop.api;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberApiController {
+
+    private final MemberService memberService;
+
+    @PostMapping("api/v1/members")
+    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @PostMapping("api/v2/members")
+    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
+
+        // 요청을 DTO로 받는 이유는 요구사항의 변경에 좀 더 유연하게 대처하기 위해서다.
+        Member member = new Member();
+        member.setName(request.getName());
+
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2(
+            @PathVariable("id") Long id,
+            @RequestBody @Valid UpdateMemberRequest request) {
+        memberService.update(id, request.getName());
+        Member findMember = memberService.findOne(id);
+        return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+    }
+
+    @GetMapping("/api/v1/members-list")
+    public List<Member> membersV1() {
+        return memberService.findMembers();
+    }
+
+    @GetMapping("/api/v2/members-list")
+    public Result membersV2() {
+        List<Member> members = memberService.findMembers();
+        List<MemberDto> collect = members.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+        return new Result(collect.size(), collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private int count;
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String name;
+    }
+
+
+
+    @Data
+    // DTO에서는 Data를 사용해도 크게 문제가 되진 않는다
+    static class UpdateMemberRequest {
+        private String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponse {
+        private Long id;
+        private String name;
+    }
+
+    @Data
+    static class CreateMemberRequest {
+        // 유지보수를 위해 DTO에 Entity의 스팩에 맞게 설계해주는것이 중요
+        @NotEmpty
+        private String name;
+    }
+
+    @Data
+    static class CreateMemberResponse {
+        private Long id;
+
+        public CreateMemberResponse(Long id) {
+            this.id = id;
+        }
+    }
+
+}
--- 스프링 부트와 JPA 활용 1/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ 스프링 부트와 JPA 활용 1/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,6 +1,8 @@
 package jpabook.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,9 +19,12 @@ public class Member {
 
 	@Id @GeneratedValue
 	private Long id;
+	@NotEmpty
 	private String name;
 	@Embedded
 	private Address address;
+	// 화면을 표시하기 위한 스펙은 엔티티에서 설정할 게 아니라 DTO로 따로 빼서 사용해야 한다
+	// @JsonIgnore
 	@OneToMany(mappedBy = "member")
 	private List<Order> orderList = new ArrayList<>();
 }
